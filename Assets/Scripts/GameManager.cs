6303183c7adba7bdc2bcd85e0579fba64af27e0e
Gilbert White <gilbert.white@stolo.com>
2018-01-26 18:16:13 -0500

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("Let's have some fun!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour 
+{
+
+	public GameObject Blocker;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Blocker.SetActive (false);
+	}
+}
