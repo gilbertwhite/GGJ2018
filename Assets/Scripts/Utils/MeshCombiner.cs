d6db560c4740c5bc555af0c6427137dc6aaf8598
Gilbert White <gilbert.white@stolo.com>
2018-01-27 14:20:35 -0500

    Sea Level Optimisation File Reorganisation Adding scripts for Sonar (not implemented)

---
+++ Assets/Scripts/Utils/MeshCombiner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshCombiner : MonoBehaviour {
+
+	public void Start()
+	{
+		MeshCollider[] meshColliders = GetComponentsInChildren<MeshCollider>();
+		CombineInstance[] combine = new CombineInstance[meshColliders.Length];
+		int i = 0;
+		while (i < meshColliders.Length) {
+			combine[i].mesh = meshColliders[i].sharedMesh;
+			combine[i].transform = meshColliders[i].transform.localToWorldMatrix;
+			meshColliders[i].gameObject.active = false;
+			i++;
+		}
+		transform.GetComponent<MeshFilter>().mesh = new Mesh();
+		transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+		transform.gameObject.active = true;
+	}
+}
