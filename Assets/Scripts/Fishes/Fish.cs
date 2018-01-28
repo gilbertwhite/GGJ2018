2cd03ead5ef0be7e73867198203b84e1797fa08d
Gilbert White <gilbert.white@stolo.com>
2018-01-27 20:31:53 -0500

    AudioClip Fix

--- Assets/Scripts/Fishes/Fish.cs
+++ Assets/Scripts/Fishes/Fish.cs
@@ -5,13 +5,39 @@ using DG.Tweening;
 
 public class Fish : MonoBehaviour
 {
+	[HideInInspector]
 	public FishData Data;
+
+	private Transform m_Target;
+	private float m_Speed = 0;
+	private float m_AttractionDelay = 0;
+	private AudioSource Audio = new AudioSource();
 
 	public void HitBySonar(Transform aTarget)
 	{
-		Data.SfxWhenHitBySonar.Play();
+		m_Target = aTarget;
+		Audio.clip = Data.SfxWhenHitBySonar;
+		Audio.Play();
+		m_AttractionDelay = Data.AttractionDuration;
 		float distance = Vector3.Distance (transform.position, aTarget.position);
-		transform.LookAt (aTarget);
-		transform.DOMove (transform.position + transform.forward * distance, Data.AttractionDuration).SetEase(Ease.InOutQuad); 
+	}
+
+	public void Update()
+	{
+		if (m_Speed <= 0)
+			return;
+		
+		transform.LookAt (m_Target);
+
+		transform.position += transform.forward * m_Speed;
+
+		float speedDiff = Data.Acceleration * Time.deltaTime;
+		if(m_AttractionDelay > 0)
+			m_Speed += speedDiff;
+		else
+			m_Speed -= speedDiff;
+			
+
+		m_AttractionDelay -= Time.deltaTime;
 	}
 }
